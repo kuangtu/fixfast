badd23af1cdf71233fb971fc65a7659a61a9195b
kuangtu <kuangtuweb@gmail.com>
2019-07-06 22:17:43 +0800

    chap10

---
+++ fast_code/openfast-1.1.2/org.book/chap10/PMAPTest.java
@@ -0,0 +1,21 @@
+package chap10;
+
+import org.openfast.template.*;
+
+import org.openfast.template.type.*;
+import org.openfast.template.operator.*;
+import org.openfast.*;
+import org.openfast.ByteUtil;
+import org.openfast.codec.FastEncoder;
+import org.openfast.template.*;
+import org.openfast.template.loader.*;
+import bookUtil.BookUtilTest;
+import bookUtil.BookUtilTest.*;
+import org.openfast.test.*;
+
+import junit.framework.TestCase;
+
+public class PMAPTest extends OpenFastTestCase {
+	
+
+}
