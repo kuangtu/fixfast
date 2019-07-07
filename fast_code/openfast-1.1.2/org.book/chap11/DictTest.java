cdfe51bcb048ad046bc91f0821e33435840012e2
kuangtu <kuangtuweb@gmail.com>
2019-07-07 11:40:25 +0800

    chap11

---
+++ fast_code/openfast-1.1.2/org.book/chap11/DictTest.java
@@ -0,0 +1,19 @@
+package chap11;
+
+import org.openfast.template.*;
+
+import org.openfast.template.type.*;
+import org.openfast.template.operator.*;
+import org.openfast.*;
+import org.openfast.codec.FastEncoder;
+import org.openfast.template.*;
+import org.openfast.template.loader.*;
+import bookUtil.BookUtilTest;
+import bookUtil.BookUtilTest.*;
+import org.openfast.test.*;
+
+import junit.framework.TestCase;
+
+public class DictTest extends OpenFastTestCase {
+
+}
