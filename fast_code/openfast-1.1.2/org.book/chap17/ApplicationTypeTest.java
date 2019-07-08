4f3c398a366bfea430cc177b85070c08732500fe
kuangtu <kuangtuweb@gmail.com>
2019-07-08 18:52:27 +0800

    chap17

---
+++ fast_code/openfast-1.1.2/org.book/chap17/ApplicationTypeTest.java
@@ -0,0 +1,26 @@
+package chap17;
+
+import org.openfast.template.*;
+
+import org.openfast.template.type.*;
+import org.openfast.template.type.codec.TypeCodec;
+import org.openfast.template.operator.*;
+
+import java.io.InputStream;
+
+import org.openfast.*;
+import org.openfast.codec.FastDecoder;
+import org.openfast.codec.FastEncoder;
+import org.openfast.template.*;
+import org.openfast.template.loader.*;
+import bookUtil.BookUtilTest;
+import bookUtil.BookUtilTest.*;
+import org.openfast.test.*;
+
+import junit.framework.TestCase;
+
+public class ApplicationTypeTest extends OpenFastTestCase {
+
+	
+
+}
