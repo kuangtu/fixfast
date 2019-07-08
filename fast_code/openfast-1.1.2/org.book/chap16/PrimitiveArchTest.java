112348935cb89420383f8a4659c0c4bc9d31f3f0
kuangtu <kuangtuweb@gmail.com>
2019-07-08 18:20:48 +0800

    chap16

---
+++ fast_code/openfast-1.1.2/org.book/chap16/PrimitiveArchTest.java
@@ -0,0 +1,24 @@
+package chap16;
+
+import org.openfast.template.*;
+
+import org.openfast.template.type.*;
+import org.openfast.template.operator.*;
+
+import java.io.InputStream;
+
+import org.openfast.*;
+import org.openfast.codec.FastDecoder;
+import org.openfast.codec.FastEncoder;
+import org.openfast.template.*;
+import org.openfast.template.loader.*;
+import bookUtil.BookUtilTest;
+import bookUtil.BookUtilTest.*;
+import org.openfast.test.*;
+
+import junit.framework.TestCase;
+
+public class PrimitiveArchTest extends OpenFastTestCase {
+
+	
+}
