f329a5a5a37c2b0272f8716ed01a1dfb7c07d3da
kuangtu <kuangtuweb@gmail.com>
2019-07-10 08:57:12 +0800

    chap19

--- fast_code/openfast-1.1.2/src/main/java/org/openfast/template/operator/TailOperatorCodec.java
+++ fast_code/openfast-1.1.2/src/main/java/org/openfast/template/operator/TailOperatorCodec.java
@@ -35,19 +35,25 @@ final class TailOperatorCodec extends OperatorCodec {
     }
 
     public ScalarValue getValueToEncode(ScalarValue value, ScalarValue priorValue, Scalar field) {
+    	//字段值为null，前值为null，传输值为null
         if (value == null) {
             if (priorValue == null)
                 return null;
+            //前值未定义且字段初始值未定义，传输值为null
             if (priorValue.isUndefined() && field.getDefaultValue().isUndefined())
                 return null;
+            //前值已指定，传输值通过NULL表示
             return ScalarValue.NULL;
         }
+        //字段值不为null，前值为null，传输值为字段值
         if (priorValue == null) {
             return value;
         }
+        //如果前值未定义，基值为初始值或者数据类型的缺省基值
         if (priorValue.isUndefined()) {
             priorValue = field.getBaseValue();
         }
+        //转为字节后计算尾部差异
         int index = 0;
         byte[] val = value.getBytes();
         byte[] prior = priorValue.getBytes();
@@ -65,14 +71,18 @@ final class TailOperatorCodec extends OperatorCodec {
 
     public ScalarValue decodeValue(ScalarValue newValue, ScalarValue previousValue, Scalar field) {
         StringValue base;
+        //字段强制类型，前值为null，D6异常
         if ((previousValue == null) && !field.isOptional()) {
             Global.handleError(FastConstants.D6_MNDTRY_FIELD_NOT_PRESENT, "");
             return null;
+        //前值为null或者未定义，基值为初始值或者数据类型的缺省基值
         } else if ((previousValue == null) || previousValue.isUndefined()) {
             base = (StringValue) field.getBaseValue();
         } else {
+        	//基值为前值
             base = (StringValue) previousValue;
         }
+        //传输值为null，可选类型字段值为null
         if ((newValue == null) || newValue.isNull()) {
             if (field.isOptional()) {
                 return null;
@@ -80,6 +90,7 @@ final class TailOperatorCodec extends OperatorCodec {
                 throw new IllegalArgumentException("");
             }
         }
+        //基于尾部计算得到字段值
         String delta = ((StringValue) newValue).value;
         int length = Math.max(base.value.length() - delta.length(), 0);
         String root = base.value.substring(0, length);
@@ -88,10 +99,12 @@ final class TailOperatorCodec extends OperatorCodec {
 
     public ScalarValue decodeEmptyValue(ScalarValue previousValue, Scalar field) {
         ScalarValue value = previousValue;
+        //前值未定义，字段值为初始值，如果初始值为未定义，字段值为null
         if (value != null && value.isUndefined())
             value = (field.getDefaultValue().isUndefined()) ? null : field.getDefaultValue();
         if (value == null && !field.isOptional())
             Global.handleError(FastConstants.D6_MNDTRY_FIELD_NOT_PRESENT, "The field " + field + " was not present.");
+        //字段值为前值
         return value;
     }
 
